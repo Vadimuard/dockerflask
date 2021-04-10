705b723d361774c1f947ff26239b1c1df634cc45
Vadym Shkurko <cn.klose5567@gmail.com>
2021-04-10 18:41:20 +0300

    all files added to separate repo for this project

---
+++ controllers/parse_request.py
@@ -0,0 +1,10 @@
+from flask import request
+
+
+def get_request_data():
+    """
+    Get keys & values from request (Note that this method should parse requests with content type "application/x-www-form-urlencoded")
+    """
+    data = request.data
+    print(data)
+    return data
---
+++ models/relations.py
@@ -0,0 +1,10 @@
+from core import db
+from sqlalchemy import Table, Column, Integer, ForeignKey
+
+# Table name -> 'association'
+# Columns: 'actor_id' -> db.Integer, db.ForeignKey -> 'actors.id', primary_key = True
+#          'movie_id' -> db.Integer, db.ForeignKey -> 'movies.id', primary_key = True
+association = Table('association', db.metadata,
+                    Column('actor_id', Integer, ForeignKey('actors.id')),
+                    Column('movie_id', Integer, ForeignKey('movies.id'))
+                    )
